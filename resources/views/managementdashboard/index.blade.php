303b7792145dfa941d48e6b32a8ba748d3ade96d
yunivent <hq.univent@gmail.com>
2023-06-22 01:05:51 +0800

    changes on management board

--- resources/views/managementdashboard/index.blade.php
+++ resources/views/managementdashboard/index.blade.php
@@ -67,8 +67,8 @@
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">
-              <i class="fas fa-chart-pie mr-1"></i>
-              Sales
+              <i class="fa-sharp fa-solid fa-circle-info mr-1"></i>
+              Information Board
             </h3>
             <div class="card-tools">
               <ul class="nav nav-pills ml-auto">
